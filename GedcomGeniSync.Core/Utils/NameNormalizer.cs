4e48c3309cb7f3034629ff90c49b5bd9c675c62f
Alexandr Makhin <amakhin@gmail.com>
2025-12-10 14:15:41 +0100

    remove copy-paste

--- GedcomGeniSync.Core/Utils/NameNormalizer.cs
+++ GedcomGeniSync.Core/Utils/NameNormalizer.cs
@@ -6,28 +6,6 @@ namespace GedcomGeniSync.Utils;
 /// </summary>
 public static class NameNormalizer
 {
-    // Cyrillic to Latin transliteration map (same as in NameVariantsService)
-    private static readonly Dictionary<char, string> CyrillicToLatin = new()
-    {
-        ['а'] = "a", ['б'] = "b", ['в'] = "v", ['г'] = "g", ['д'] = "d",
-        ['е'] = "e", ['ё'] = "yo", ['ж'] = "zh", ['з'] = "z", ['и'] = "i",
-        ['й'] = "y", ['к'] = "k", ['л'] = "l", ['м'] = "m", ['н'] = "n",
-        ['о'] = "o", ['п'] = "p", ['р'] = "r", ['с'] = "s", ['т'] = "t",
-        ['у'] = "u", ['ф'] = "f", ['х'] = "kh", ['ц'] = "ts", ['ч'] = "ch",
-        ['ш'] = "sh", ['щ'] = "shch", ['ъ'] = "", ['ы'] = "y", ['ь'] = "",
-        ['э'] = "e", ['ю'] = "yu", ['я'] = "ya",
-        // Ukrainian specific
-        ['і'] = "i", ['ї'] = "yi", ['є'] = "ye", ['ґ'] = "g",
-        // Upper case
-        ['А'] = "A", ['Б'] = "B", ['В'] = "V", ['Г'] = "G", ['Д'] = "D",
-        ['Е'] = "E", ['Ё'] = "Yo", ['Ж'] = "Zh", ['З'] = "Z", ['И'] = "I",
-        ['Й'] = "Y", ['К'] = "K", ['Л'] = "L", ['М'] = "M", ['Н'] = "N",
-        ['О'] = "O", ['П'] = "P", ['Р'] = "R", ['С'] = "S", ['Т'] = "T",
-        ['У'] = "U", ['Ф'] = "F", ['Х'] = "Kh", ['Ц'] = "Ts", ['Ч'] = "Ch",
-        ['Ш'] = "Sh", ['Щ'] = "Shch", ['Ъ'] = "", ['Ы'] = "Y", ['Ь'] = "",
-        ['Э'] = "E", ['Ю'] = "Yu", ['Я'] = "Ya",
-        ['І'] = "I", ['Ї'] = "Yi", ['Є'] = "Ye", ['Ґ'] = "G"
-    };
 
     /// <summary>
     /// Normalize a name for comparison (transliterate, lowercase, remove punctuation)
@@ -63,7 +41,7 @@ public static class NameNormalizer
 
         foreach (var c in text)
         {
-            if (CyrillicToLatin.TryGetValue(c, out var replacement))
+            if (TransliterationConstants.CyrillicToLatin.TryGetValue(c, out var replacement))
             {
                 result.Append(replacement);
             }
